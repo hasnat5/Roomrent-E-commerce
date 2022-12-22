17dec4a6798866f58dd5faf39ded82d8f7aae9e5
hasnat5 <hasnatferdiananda5@gmail.com>
2022-12-22 18:46:59 +0700

    change to stateful component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            first
+        }
+    }
+
     render() {
         return (
             <div>Login dek</div>
