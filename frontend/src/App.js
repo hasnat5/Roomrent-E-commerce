f4c694583f2690e10de602144f475347b7717c39
hasnat5 <hasnatferdiananda5@gmail.com>
2022-12-22 17:18:17 +0700

    adding pages and component folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div>
-      <p className="text-red-500">tes</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Homepage extends Component {
+    render() {
+        return (
+            <div><p className="text-red-500">homepage</p></div>
+        )
+    }
+}
