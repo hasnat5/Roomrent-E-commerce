317a21e0704056414a9674537df4a56438c6033d
FadhliFathoni <fadhlifathoni62@gmail.com>
2022-12-28 19:06:56 +0700

    add env

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Login from './pages/Login';
+import React from 'react';
 
 function App() {
   return (
